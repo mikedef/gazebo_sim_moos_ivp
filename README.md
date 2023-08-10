6b9ff333764122e92e209571c8ca46d3e73e8466
mikedef <mikedef@mit.edu>
2023-08-10 14:40:44 -0400

    init punch simulation

---
+++ README.md
@@ -0,0 +1,8 @@
+# aurora_punch_gazebo
+Repository for all gazebo related items for the Aurora: Falcon PUNCH project
+
+## Notes
+* 
+
+## TODO:
+* Remove all of the extra files that are not really needed for this project. 
---
+++ punch_gazebo/worlds/clean.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#---------------------------------------------------
+#  Script: clean.sh
+#  Author: Michael DeFilippo, adapted from moos-ivp clean.sh (Michael Benjamin)
+#    Date: Jan 2023
+#---------------------------------------------------
+#  Part 1: Declare global var defaults                                                                  
+#----------------------------------------------------------                                             
+VERBOSE=""                                                                                              
+                                                                                                        
+#-------------------------------------------------------                                                
+#  Part 1: Check for and handle command-line arguments                                                  
+#-------------------------------------------------------                                                
+for ARGI; do                                                                                            
+    if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ]; then                                               
+        echo "clean.sh [SWITCHES]        "                                                              
+        echo "   --verbose, -v           "                                                              
+        echo "   --help, -h              "                                                              
+    elif [ "${ARGI}" = "--verbose" -o "${ARGI}" = "-v" ]; then                                          
+        VERBOSE="-v"                                                                                    
+    else                                                                                                
+        echo "clean.sh: Bad Arg:" $ARGI                                                                 
+        exit 1                                                                                          
+    fi                                                                                                  
+done                                                                                                    
+                                                                                                        
+#-------------------------------------------------------                                                
+#  Part 2: Do the cleaning!                                                                             
+#-------------------------------------------------------                                                
+if [ "${VERBOSE}" = "-v" ]; then                                                                        
+    echo "Cleaning: $PWD"                                                                               
+fi                                                                                                      
+
+# clean emacs leftovers 
+rm -f   $VERBOSE   *~ .\#* \#*\# 
+
+
+                                               
